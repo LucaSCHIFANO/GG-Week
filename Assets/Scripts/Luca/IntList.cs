6309ceef5a0d99221f011a4d49120eab92d3dd87
LucaSchifano <luca.schifano005@gmail.com>
2021-04-13 11:09:21 +0200

    Arbre de questions random

---
+++ Assets/Scripts/Luca/IntList.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IntList : MonoBehaviour
+{
+    public List<int> list = new List<int>();
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
