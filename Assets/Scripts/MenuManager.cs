62c8f376b33ce1b3399050d5e16153a0c3b4740a
nguyen <virginie.nguyen@edu.devinci.fr>
2021-04-14 01:31:45 +0200

    feat : pause manager

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -3,12 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class MenuManager : MonoBehaviour
-{
-    public GameObject menu;
-    public GameObject options;
-    public GameObject sounds;
-    public GameObject videos;
-    
+{   
     public void OpenSubMenu(GameObject name)
     {
         name.SetActive(true);
