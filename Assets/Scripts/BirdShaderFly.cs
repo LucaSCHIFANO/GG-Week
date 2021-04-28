8e5e80fc0df6a0dc4fd6eda55d3d4bb3b4626cd6
nguyen <virginie.nguyen@edu.devinci.fr>
2021-04-28 12:13:11 +0200

    fix (sound) : not the good sound for fast beating ?

--- Assets/Scripts/BirdShaderFly.cs
+++ Assets/Scripts/BirdShaderFly.cs
@@ -5,14 +5,27 @@ using UnityEngine;
 public class BirdShaderFly : MonoBehaviour
 {
     public float speed = 10.0f;
-
+    private Vector2 startPos;
     private void Start()
     {
+        startPos = transform.position;
         FindObjectOfType<SoundManager>().PlaySound("Seagull");
     }
     // Update is called once per frame
     void Update()
     {
-        transform.position = new Vector3(transform.position.x + Time.deltaTime * speed, transform.position.y + speed * Time.deltaTime, transform.position.z);
+        Camera cam = Camera.main;
+        float camHeight = 2.0f * cam.orthographicSize;
+        float posY = transform.position.y;
+        if(posY < camHeight * 4.0f)
+        {
+            transform.position = new Vector3(transform.position.x + Time.deltaTime * speed, transform.position.y + speed * Time.deltaTime, transform.position.z);
+        }
+        else
+        {
+            transform.position = startPos;
+            FindObjectOfType<SoundManager>().PlaySound("Seagull");
+        }
+
     }
 }
